69cdfe8a97c3d574d684a0384cef8ac29599ae94
H T Watkinson <k1914426@kingston.ac.uk>
2021-06-26 13:02:41 +0100

    10/02/21 - Using Strings in Java to create a tree.

---
+++ src/Tree.java
@@ -0,0 +1,22 @@
+// 10/02/21
+
+//  Here I am defining the tree class in this file!
+
+public class Tree
+{
+    public static void main(String[] args)
+    {
+        // 10/02/21 - Introduce ourselves and printing a tree to the screen.
+
+        System.out.println("Hey there, I'm Harry and I am learing to code in Java!");
+
+        System.out.println(" I’m going to plant a tree today!");
+
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+
+    }
+}
